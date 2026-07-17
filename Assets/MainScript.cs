0b58c27b32c6e91d95c14f8e34a4c47bc48f7a87
John Burak <john.burak@gmail.com>
2026-07-17 13:51:41 -0600

    Initial commit

---
+++ Assets/MainScript.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainScript : MonoBehaviour
+{
+    [SerializeField] TMP_InputField inputField;
+    [SerializeField] Button saveButton;
+    [SerializeField] Button loadButton;
+}
